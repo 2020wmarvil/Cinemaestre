6d8eb468a1d1f844b30d249b129b0dbcfd9f3202
Wyatt Marvil <2020wmarvil@gmail.com>
2021-12-05 12:09:06 -0500

    Import leantween

--- CameraTool/Assets/CinemaestreCamera.cs
+++ CameraTool/Assets/CinemaestreCamera.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class CinemaestreCamera : MonoBehaviour {
-	Vector3 pos;
+// TODO: 
+// [ ] Pan (in cardinal directions)
+// [ ] Zoom (in and out with an adjustable time rate)
+// [ ] Fade Screen (using hdrp)
+// [ ] Lerp FOV  (in and out with an adjustable time rate)
+// [ ] Trigger/ Enable / Disable GameObjects or Functions
 
-	private void Awake() {
-		pos = transform.position;
-	}
+// enum to choose mode
+// editor depending on that enum
+// someway to trigger event
+// ability to interact from script
 
-	private void Update() {
-		transform.position = pos + new Vector3(0f, Mathf.Sin(Time.time), 0f);
+namespace CinemaestreCamera {
+	public class CinemaestreCamera : MonoBehaviour {
+		Vector3 pos;
+	
+		private void Awake() {
+			pos = transform.position;
+		}
+	
+		private void Update() {
+			if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+				transform.position = pos + new Vector3(0f, Mathf.Sin(Time.time), 0f);
+	
+				LeanTween.value(0f, 1f, 1f)
+				 .setOnUpdate((float value) => {
+					 transform.position = pos + new Vector3(0f, value, 0f);
+				 }).setLoopPingPong()
+				 .setEaseInOutCubic();
+			}
+		}
 	}
 }
+
